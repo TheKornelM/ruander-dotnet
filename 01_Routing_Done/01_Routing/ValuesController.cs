2d61a245954ba80df240ab1f187d4408dd62c9fd
TheKornelM <makari.kornel@gmail.com>
2026-05-30 18:29:24 +0200

    Remove MVC HomeController and add basic API controller
    Removed HomeController and its MVC actions. Registered controllers in Program.cs. Added ValuesController with basic GET endpoints for API functionality.

---
+++ 01_Routing_Done/01_Routing/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _01_Routing
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET <ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET <ValuesController>/5
+        [HttpGet("{id:int}")]
+        public string Get(int id)
+        {
+            return $"value{id}";
+        }
+    }
+}
